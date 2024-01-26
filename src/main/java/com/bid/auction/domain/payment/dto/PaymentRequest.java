6d237bed8e7dc6588a4624c3b98e5b50425a0ff2
Minjae-An <mj3242@naver.com>
2024-01-26 21:54:10 +0900

    feat: 결제 완료 처리 API 관련 요청 DTO 구현(#5)
    - PG서버로부터 웹훅을 통해 받는 결제 주문 DTO
    - 결제 위변조 검증 및 완료 처리 요청 DTO
    
    # [chore]	코드 수정, 내부 파일 수정
    # [feat]	새로운 기능 구현
    # [add]	FEAT 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성
    # [hotfix]	issue나 QA에서 급한 버그 수정에 사용
    # [fix]	버그, 오류 해결
    # [del]	쓸모 없는 코드 삭제
    # [docs]	README나 WIKI 등의 문서 개정
    # [correct]	주로 문법의 오류나 타입의 변경, 이름 변경에 사용
    # [move]	프로젝트 내 파일이나 코드의 이동
    # [rename]	파일 이름 변경이 있을 때 사용
    # [improve]	향상이 있을 때 사용
    # [refactor]	전면 수정이 있을 때 사용
    # [test]	테스트 코드 추가 시 사용

--- src/main/java/com/bid/auction/domain/payment/dto/PaymentRequest.java
+++ src/main/java/com/bid/auction/domain/payment/dto/PaymentRequest.java
@@ -1,14 +1,30 @@
 package com.bid.auction.domain.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.ToString;
 
 public class PaymentRequest {
-    @Getter
-    public static class PaymentVerificationRequest{
-        @NotNull
-        private String impUid;
-        @NotNull
-        private String merchantUid;
-    }
+	@Getter
+	public static class PaymentVerificationRequest {
+		@NotNull
+		@JsonProperty("imp_uid")
+		private String impUid;
+		@NotNull
+		@JsonProperty("merchant_uid")
+		private String merchantUid;
+	}
+
+	@Getter
+	@ToString
+	public static class PaymentOrderResult {
+		@JsonProperty("imp_uid")
+		private String pgPaymentOrderUid;
+		@JsonProperty("merchant_uid")
+		private String merchantPaymentOrderUid;
+		@JsonProperty("status")
+		private String status;
+	}
 }
