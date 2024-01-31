cb327881342f6fb4e40f57988f0e5c035174d40c
Minjae-An <mj3242@naver.com>
2024-01-31 16:52:36 +0900

    feat: 결제 엔티티 Query 담당 Service 구현(#5)

---
+++ src/main/java/com/bid/auction/domain/payment/service/PaymentQueryService.java
@@ -0,0 +1,45 @@
+package com.bid.auction.domain.payment.service;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bid.auction.domain.payment.dto.PaymentRequest.PaymentVerificationRequest;
+import com.bid.auction.domain.payment.dto.PaymentVerificationResponse;
+import com.bid.auction.domain.payment.entity.PaymentOrder;
+import com.bid.auction.domain.payment.repository.PaymentOrderRepository;
+import com.bid.auction.global.enums.statuscode.ErrorStatus;
+import com.bid.auction.global.exception.GeneralException;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PaymentQueryService {
+	private final IamportService iamportService;
+	private final PaymentOrderRepository paymentOrderRepository;
+
+	public void verifyPaymentOrder(PaymentVerificationRequest request) throws IamportResponseException, IOException {
+		PaymentVerificationResponse verificationResponse = iamportService.getPaymentVerificationResponse(request);
+		String uid = verificationResponse.merchantPaymentOrderUid();
+		PaymentOrder paymentOrder = findPaymentOrderByMerchantUid(uid);
+		verifyActualAmountEqualOrderAmount(verificationResponse, paymentOrder);
+	}
+
+	public PaymentOrder findPaymentOrderByMerchantUid(String uid) {
+		return paymentOrderRepository.findByMerchantUid(uid)
+			.orElseThrow(() -> new GeneralException(ErrorStatus._PAYMENT_ORDER_NOT_FOUND));
+	}
+
+	private void verifyActualAmountEqualOrderAmount(PaymentVerificationResponse response, PaymentOrder paymentOrder) {
+		Integer orderAmount = paymentOrder.getPaymentOrderAmount();
+		Integer actualAmount = response.paymentOrderAmount();
+
+		if (!actualAmount.equals(orderAmount)) {
+			throw new GeneralException(ErrorStatus._UNEQUAL_ACTUAL_PAYMENT_AMOUNT);
+		}
+	}
+}
