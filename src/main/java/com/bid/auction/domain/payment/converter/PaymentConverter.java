cdeb229e7ae12b1482bdbbf5bb901ad45e0812a3
Minjae-An <mj3242@naver.com>
2024-01-31 16:50:57 +0900

    feat: 결제 엔티티-DTO 매핑 로직 구현(#5)
    이하 로직을 구현하였다.
    - PG사 결제 데이터 바탕 PaymentOrder 생성 로직
    - 결제 주문, PG사 결제 데이터 바탕 Payment 생성 로직
    - PG사 결제 데이터 바탕 결제 위변조 응답 DTO 생성 로직
    - 결제 완료 응답 DTO 생성 로직
    
    - PG사의 Date 필드 LocalDateTime 매핑 로직
    - PG사 status 값, PaymentOrderStatus 매핑 로직
    - PG사 method 값, PaymentMethod 매핑 로직

---
+++ src/main/java/com/bid/auction/domain/payment/converter/PaymentConverter.java
@@ -0,0 +1,83 @@
+package com.bid.auction.domain.payment.converter;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
+
+import com.bid.auction.domain.payment.dto.PaymentResponse.PaymentCompleteResponse;
+import com.bid.auction.domain.payment.dto.PaymentVerificationResponse;
+import com.bid.auction.domain.payment.entity.Payment;
+import com.bid.auction.domain.payment.entity.PaymentOrder;
+import com.bid.auction.domain.payment.enums.PaymentMethod;
+import com.bid.auction.domain.payment.enums.PaymentOrderStatus;
+import com.bid.auction.domain.user.User;
+import com.bid.auction.global.enums.statuscode.ErrorStatus;
+import com.bid.auction.global.exception.GeneralException;
+
+public class PaymentConverter {
+	public static PaymentOrder toPaymentOrder(com.siot.IamportRestClient.response.Payment response, User orderingUser) {
+		return PaymentOrder.builder()
+			.pgUid(response.getImpUid())
+			.merchantUid(response.getMerchantUid())
+			.paymentOrderAmount(response.getAmount().intValue())
+			.orderStatus(convertPaymentStatus(response.getStatus()))
+			.requestedAt(toLocalDateTime(response.getPaidAt()))
+			.orderingUser(orderingUser)
+			.build();
+	}
+
+	public static Payment generatePayment(PaymentOrder paymentOrder,
+		com.siot.IamportRestClient.response.Payment pgPayment) {
+		return Payment.builder()
+			.amount(paymentOrder.getPaymentOrderAmount())
+			.requestedAt(paymentOrder.getRequestedAt())
+			.approvedAt(toLocalDateTime(pgPayment.getPaidAt()))
+			.merchantPaymentUid(paymentOrder.getMerchantUid())
+			.pgPaymentUid(paymentOrder.getPgUid())
+			.paymentMethod(convertPaymentMethod(pgPayment.getPayMethod()))
+			.paymentOrder(paymentOrder)
+			.paidUser(paymentOrder.getOrderingUser())
+			.build();
+	}
+
+	public static PaymentVerificationResponse toPaymentVerificationResponse(
+		com.siot.IamportRestClient.response.Payment response) {
+		return PaymentVerificationResponse.builder()
+			.pgPaymentOrderUid(response.getImpUid())
+			.merchantPaymentOrderUid(response.getMerchantUid())
+			.paymentOrderAmount(response.getAmount().intValue())
+			.requestedAt(toLocalDateTime(response.getPaidAt()))
+			.build();
+	}
+
+	public static PaymentCompleteResponse toPaymentCompleteResponse(Payment payment) {
+		return PaymentCompleteResponse.builder()
+			.paymentId(payment.getId())
+			.paymentAmount(payment.getAmount())
+			.paymentMethod(payment.getPaymentMethod())
+			.requestedAt(payment.getRequestedAt())
+			.approvedAt(payment.getApprovedAt())
+			.pgPaymentOrderUid(payment.getPgPaymentUid())
+			.merchantPaymentOrderUid(payment.getMerchantPaymentUid())
+			.build();
+	}
+
+	private static LocalDateTime toLocalDateTime(Date date) {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+
+	private static PaymentOrderStatus convertPaymentStatus(String status) {
+		return Arrays.stream(PaymentOrderStatus.values())
+			.filter(paymentOrderStatus -> paymentOrderStatus.getStatus().equals(status))
+			.findFirst()
+			.orElseThrow(() -> new GeneralException(ErrorStatus._PAYMENT_ORDER_STATUS_NOT_FOUND));
+	}
+
+	private static PaymentMethod convertPaymentMethod(String method) {
+		return Arrays.stream(PaymentMethod.values())
+			.filter(paymentMethod -> paymentMethod.getMethod().equals(method))
+			.findFirst()
+			.orElseThrow(() -> new GeneralException(ErrorStatus._PAYMENT_METHOD_NOT_FOUND));
+	}
+}
