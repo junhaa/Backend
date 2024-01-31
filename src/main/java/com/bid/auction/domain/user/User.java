4a3e79f437c399c6cda57581b38aef42d6f4a4be
Minjae-An <mj3242@naver.com>
2024-01-31 16:31:32 +0900

    feat: 결제 기능 구현을 위한 임의 사용자 엔티티 구현(#5)

---
+++ src/main/java/com/bid/auction/domain/user/User.java
@@ -0,0 +1,36 @@
+package com.bid.auction.domain.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bid.auction.domain.payment.entity.Payment;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@OneToMany(mappedBy = "paidUser", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Payment> userPayments = new ArrayList<>();
+}
