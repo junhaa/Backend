b437bf7827098563ffdfb3c4b9a6e20a568b78c4
Minjae-An <mj3242@naver.com>
2024-02-02 13:04:02 +0900

    feat: 결제 위변조 요청 DTO, API 명세(#5)

--- src/main/java/com/bid/auction/domain/payment/dto/PaymentRequest.java
+++ src/main/java/com/bid/auction/domain/payment/dto/PaymentRequest.java
@@ -2,6 +2,7 @@ package com.bid.auction.domain.payment.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.ToString;
@@ -9,9 +10,12 @@ import lombok.ToString;
 public class PaymentRequest {
 	@Getter
 	public static class PaymentVerificationRequest {
+		@Schema(name = "IMP Uid", type = "string", example = "imp_245402750188", description = "결제 요청에 PG사에서 부여한 고유번호")
 		@NotNull
 		@JsonProperty("imp_uid")
 		private String impUid;
+		@Schema(name = "Mechant Uid", type = "string", example = "1_productOrder_uuid18",
+			description = "결제 요청에 서비스에서 부여한 고유번호")
 		@NotNull
 		@JsonProperty("merchant_uid")
 		private String merchantUid;
@@ -20,6 +24,7 @@ public class PaymentRequest {
 	@Getter
 	@ToString
 	public static class PaymentOrderResult {
+
 		@JsonProperty("imp_uid")
 		private String pgPaymentOrderUid;
 		@JsonProperty("merchant_uid")
