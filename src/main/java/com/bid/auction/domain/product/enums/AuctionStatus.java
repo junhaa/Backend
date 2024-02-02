469868c2bf275660346ce6e4c1ca6e64f81a2f99
junhaa <2171326@hansung.ac.kr>
2024-02-02 14:14:45 +0900

    :sparkles: FEAT. 경매 게시글 관련 Enum 추가 #6

---
+++ src/main/java/com/bid/auction/domain/product/enums/AuctionStatus.java
@@ -0,0 +1,5 @@
+package com.bid.auction.domain.product.enums;
+
+public enum AuctionStatus {
+	_ACTIVE, _INACTIVE, _ENDED
+}
---
+++ src/main/java/com/bid/auction/domain/product/enums/ProductCategoryName.java
@@ -0,0 +1,5 @@
+package com.bid.auction.domain.product.enums;
+
+public enum ProductCategoryName {
+	_TOPS, _BOTTOMS, _OUTERWEAR, _FOOTWEAR, _BAG_AND_LUGGAGE, _ACCESSORIES;
+}
