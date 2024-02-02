ec0b830d9d54521f17f18f6a28c1ca571336a275
Minjae-An <mj3242@naver.com>
2024-02-02 13:04:45 +0900

    feat: 결제 완료 응답 DTO, API 명세(#5)

--- src/main/java/com/bid/auction/domain/payment/dto/PaymentResponse.java
+++ src/main/java/com/bid/auction/domain/payment/dto/PaymentResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.bid.auction.domain.payment.enums.PaymentMethod;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,12 +17,21 @@ public class PaymentResponse {
 	@AllArgsConstructor
 	@Builder
 	public static class PaymentCompleteResponse {
+		@Schema(name = "Payment ID", type = "int", example = "1", description = "결제 데이터 PK")
 		private Long paymentId;
+		@Schema(name = "Payment Amount", type = "int", example = "4000", description = "결제 금액")
 		private Integer paymentAmount;
+		@Schema(name = "Payment Method", type = "string", example = "card", description = "결제 수단")
 		private PaymentMethod paymentMethod;
+		@Schema(name = "Requested At", type = "string", example = "2024-02-01T15:30:00", description = "결제 요청 일시")
 		private LocalDateTime requestedAt;
+		@Schema(name = "Approved At", type = "string", example = "2024-02-01T15:30:00", description = "결제 승인 일시")
 		private LocalDateTime approvedAt;
+		@Schema(name = "PG Payment Order ID", type = "string", example = "imp_245402750188",
+			description = "처리 완료된 결제 PG사 고유번호")
 		private String pgPaymentOrderUid;
+		@Schema(name = "Merchant Payment Order ID", type = "string", example = "1_productOrder_uuid18",
+			description = "처리 완료된 결제 서비스 고유번호")
 		private String merchantPaymentOrderUid;
 	}
 }
