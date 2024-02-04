5f416be10ff09df29296f12770bd47fcd6d02a88
eojin0814 <leeeojin6@gmail.com>
2024-02-04 22:02:20 +0900

    [feat]Oauth Kako service

--- src/main/java/com/bid/auction/domain/user/application/KakaoAuthService.java
+++ src/main/java/com/bid/auction/domain/user/application/KakaoAuthService.java
@@ -14,10 +14,10 @@ public class KakaoAuthService {
     private final KakaoUserInfo kakaoUserInfo;
     private final UserRepository userRepository;
 
-    @Transactional(readOnly = true)
-    public Long isSignedUp(String token) {
-        KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(token);
-        User user = userRepository.findByKeyCode(userInfo.getId().toString()).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
-        return user.getId();
-    }
+//    @Transactional(readOnly = true)
+//    public Long isSignedUp(String token) {
+//        KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(token);
+//        User user = userRepository.findByKeyCode(userInfo.getId().toString()).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+//        return user.getId();
+//    }
 }
