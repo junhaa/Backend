6b6f2fc0c567c601cdc6e25c29f525087ee11f7c
Minjae-An <mj3242@naver.com>
2024-02-05 22:04:26 +0900

    feat: 서비스 고유 코드 기반 결제 조회 로직 구현(#11)

--- src/main/java/com/bid/auction/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/bid/auction/domain/payment/repository/PaymentRepository.java
@@ -1,8 +1,11 @@
 package com.bid.auction.domain.payment.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bid.auction.domain.payment.entity.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+	Optional<Payment> findByMerchantPaymentUid(String uid);
 }
