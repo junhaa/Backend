dfae132ceffe9c982a1bca26f1fb578dc286c06a
junhaa <2171326@hansung.ac.kr>
2024-02-06 04:18:44 +0900

    :sparkles: FEAT. AuctionPost Repository 기능 추가 #6

--- src/main/java/com/bid/auction/domain/product/repository/AuctionPostRepository.java
+++ src/main/java/com/bid/auction/domain/product/repository/AuctionPostRepository.java
@@ -1,8 +1,17 @@
 package com.bid.auction.domain.product.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bid.auction.domain.product.entity.AuctionPost;
+import com.bid.auction.domain.product.enums.AuctionStatus;
+import com.bid.auction.domain.product.enums.ProductCategoryGender;
+import com.bid.auction.domain.product.enums.ProductCategoryName;
 
 public interface AuctionPostRepository extends JpaRepository<AuctionPost, Long> {
+	Page<AuctionPost> findByAuctionStatus(AuctionStatus auctionStatus, Pageable pageable);
+
+	Page<AuctionPost> findByProductCategoryProductCategoryGenderAndProductCategoryProductCategoryNameAndAuctionStatus(
+		ProductCategoryGender gender, ProductCategoryName categoryName, AuctionStatus auctionStatus, Pageable pageable);
 }
