b056cb84bc981830ba1689091e56910ebea362e3
Minjae-An <mj3242@naver.com>
2024-02-07 17:22:31 +0900

    feat: 환불 처리내 로직들 별도 트랜잭션으로 분리(#11)

--- src/main/java/com/bid/auction/domain/refund/service/RefundCommandService.java
+++ src/main/java/com/bid/auction/domain/refund/service/RefundCommandService.java
@@ -12,11 +12,12 @@ import com.bid.auction.domain.payment.service.IamportService;
 import com.bid.auction.domain.payment.service.PaymentQueryService;
 import com.bid.auction.domain.refund.dto.RefundRequestOrder;
 import com.bid.auction.domain.refund.dto.RefundResponse.PgRefundResponse;
-import com.bid.auction.domain.refund.dto.RefundResponse.RefundSuccessResponse;
 import com.bid.auction.domain.refund.entity.Refund;
 import com.bid.auction.domain.refund.entity.RefundRequest;
 import com.bid.auction.domain.refund.repository.RefundRepository;
 import com.bid.auction.domain.refund.repository.RefundRequestRepository;
+import com.bid.auction.global.enums.statuscode.ErrorStatus;
+import com.bid.auction.global.exception.GeneralException;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 
 import lombok.RequiredArgsConstructor;
@@ -26,25 +27,11 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class RefundCommandService {
 	private final IamportService iamportService;
-	private final PaymentQueryService paymentQueryService;
 	private final RefundRequestRepository refundRequestRepository;
 	private final RefundRepository refundRepository;
+	private final PaymentQueryService paymentQueryService;
 
-	public RefundSuccessResponse refund(RefundRequestOrder request, LocalDateTime requestedAt)
-		throws IamportResponseException, IOException {
-		String merchantUid = request.getMerchantUid();
-		Payment payment = paymentQueryService.findPaymentByMerchantUid(merchantUid);
-
-		RefundRequest refundRequest = saveRefundRequest(payment, request, requestedAt);
-
-		PgRefundResponse pgRefundResponse = iamportService
-			.requestRefund(payment.getPgPaymentUid(), refundRequest);
-		Refund refund = saveRefund(refundRequest, pgRefundResponse);
-
-		return RefundConverter.toRefundSuccessResponse(refund);
-	}
-
-	private RefundRequest saveRefundRequest(
+	public RefundRequest saveRefundRequest(
 		Payment payment,
 		RefundRequestOrder request,
 		LocalDateTime requestedAt
@@ -54,7 +41,19 @@ public class RefundCommandService {
 		return refundRequestRepository.save(refundRequest);
 	}
 
-	private Refund saveRefund(RefundRequest request, PgRefundResponse response) {
+	public PgRefundResponse requestRefund(Payment payment, RefundRequest request) throws
+		IamportResponseException,
+		IOException {
+		return iamportService.requestRefund(payment.getPgPaymentUid(), request);
+	}
+
+	public Refund saveRefund(PgRefundResponse response) {
+		Payment payment = paymentQueryService
+			.findPaymentByMerchantUid(response.getMerchantUid());
+		RefundRequest request = refundRequestRepository
+			.findByPayment(payment)
+			.orElseThrow(() -> new GeneralException(ErrorStatus._REFUND_REQUEST_NOT_FOUND));
+		request.complete();
 		Refund refund = RefundConverter.toRefund(request, response);
 		return refundRepository.save(refund);
 	}
