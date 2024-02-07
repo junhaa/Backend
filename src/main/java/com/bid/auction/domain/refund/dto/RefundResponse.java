51886fd1d7ea29d47c112ef034b9befb75e3e575
Minjae-An <mj3242@naver.com>
2024-02-07 20:42:45 +0900

    add: 기본 명세 스키마 구성(#11)

--- src/main/java/com/bid/auction/domain/refund/dto/RefundResponse.java
+++ src/main/java/com/bid/auction/domain/refund/dto/RefundResponse.java
@@ -2,6 +2,7 @@ package com.bid.auction.domain.refund.dto;
 
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,10 +24,35 @@ public class RefundResponse {
 	@AllArgsConstructor
 	@Builder
 	public static class RefundSuccessResponse {
+		@Schema(
+			type = "int",
+			example = "1",
+			description = "환불 데이터의 PK"
+		)
 		private Long refundId;
+		@Schema(
+			type = "string",
+			example = "imp_245402750188",
+			description = "처리 완료된 결제 PG사 고유번호"
+		)
 		private String pgUid;
+		@Schema(
+			type = "string",
+			example = "1_productOrder_uuid18",
+			description = "처리 완료된 결제 서비스 고유번호"
+		)
 		private String merchantUid;
+		@Schema(
+			type = "int",
+			example = "1000",
+			description = "환불 처리 완료 금액"
+		)
 		private Integer amount;
+		@Schema(
+			type = "string",
+			example = "2024-02-01T15:30:00",
+			description = "환불 완료 일시"
+		)
 		private LocalDateTime refundedAt;
 	}
 }
