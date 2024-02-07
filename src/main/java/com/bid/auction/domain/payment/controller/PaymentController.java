5167202dd145093c5fb83897d18c4436d9aecba7
Minjae-An <mj3242@naver.com>
2024-02-07 21:06:18 +0900

    add: 결제 완료 API, 예외 응답별 명세 추가(#11)
    - 해당 결제 주문(요청) 데이터가 존해하지 않을 경우
    - 실 결제 금액과 결제 주문 금액이 다를 경우

--- src/main/java/com/bid/auction/domain/payment/controller/PaymentController.java
+++ src/main/java/com/bid/auction/domain/payment/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.bid.auction.domain.payment.controller;
 
 import java.io.IOException;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,6 @@ import com.bid.auction.domain.payment.dto.PaymentRequest.PaymentVerificationRequ
 import com.bid.auction.domain.payment.dto.PaymentResponse.PaymentCompleteResponse;
 import com.bid.auction.domain.payment.service.PaymentCommandService;
 import com.bid.auction.domain.payment.service.PaymentQueryService;
-import com.bid.auction.global.enums.statuscode.ApiErrorCode;
-import com.bid.auction.global.enums.statuscode.ErrorStatus;
 import com.bid.auction.global.enums.statuscode.SuccessStatus;
 import com.bid.auction.global.response.ApiResponse;
 import com.siot.IamportRestClient.exception.IamportResponseException;
@@ -22,6 +21,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -35,26 +35,36 @@ public class PaymentController {
 	private final PaymentCommandService paymentCommandService;
 
 	@PostMapping("/payment-order")
-	@Operation(summary = "PG 서버에서 결제 요청 결과를 받는 웹훅 API", description = """
-		클라이언트에서 결제를 요청할 경우, PG사 서버에 그 결과를 받는 웹훅 API입니다.
-		프론트에서 이용하는 API가 아닙니다. 다만, 프론트에서 merchant_uid의 포맷을
-		잘못 보낼 경우 이 API에서 예외가 발생할 수 있습니다.
-		""")
+	@Operation(
+		summary = "PG 서버에서 결제 요청 결과를 받는 웹훅 API",
+		description = """
+			클라이언트에서 결제를 요청할 경우, PG사 서버에 그 결과를 받는 웹훅 API입니다.
+			프론트에서 이용하는 API가 아닙니다. 다만, 프론트에서 merchant_uid의 포맷을
+			잘못 보낼 경우 이 API에서 예외가 발생할 수 있습니다.
+			"""
+	)
 	@Parameters({
-		@Parameter(name = "imp_uid", description = "PG사에서 부여하는 결제 데이터 고유번호", example = "imp_245402750188"),
+		@Parameter(
+			name = "imp_uid",
+			description = "PG사에서 부여하는 결제 데이터 고유번호",
+			example = "imp_245402750188"),
 		@Parameter(name = "merchant_uid",
 			description = "서비스 서버에서 부여하는 결제 데이터 고유번호, {User PK}\\_{Payment Type}\\_{uid} 형식",
 			example = "1_productOrder_uuid18"),
-		@Parameter(name = "status", description = "PG사 결제 요청 처리 상태", example = "paid")
+		@Parameter(name = "status",
+			description = "PG사 결제 요청 처리 상태",
+			example = "paid")
 	})
-	@ApiErrorCode(ErrorStatus.class)
 	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON204",
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "COMMON204",
 			description = "응답 데이터 없음, 정상 처리",
 			content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER4001",
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "USER4001",
 			description = "merchant_uid의 형식이 잘못 되었거나 해당 ID를 가진 사용자가 존재하지 않음",
-			content = @Content(schema = @Schema(implementation = ApiResponse.class)))})
+			content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+	})
 	public ApiResponse<Void> savePaymentOrder(@RequestBody PaymentOrderResult orderResult) throws
 		IamportResponseException,
 		IOException {
@@ -69,14 +79,71 @@ public class PaymentController {
 			비교하는 결제 위변조 검증 과정을 포함합니다.
 			""")
 	@Parameters({
-		@Parameter(name = "imp_uid", description = "PG사에서 부여하는 결제 데이터 고유번호", example = "imp_245402750188"),
-		@Parameter(name = "merchant_uid",
+		@Parameter(
+			name = "imp_uid",
+			description = "PG사에서 부여하는 결제 데이터 고유번호",
+			example = "imp_245402750188"),
+		@Parameter(
+			name = "merchant_uid",
 			description = "서비스 서버에서 부여하는 결제 데이터 고유번호, {User PK}_{Payment Type}_{uid} 형식",
 			example = "1_productOrder_uuid18")
 	})
-	@ApiErrorCode(ErrorStatus.class)
-	@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",
-		description = "정상 결제 완료 처리")
+	@ApiResponses({
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "200",
+			description = "정상 결제 완료 처리"),
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "PAYMENT_ORDER4001",
+			description = "해당 결제 주문이 존재하지 않습니다.",
+			content = @Content(
+				mediaType = MediaType.APPLICATION_JSON_VALUE,
+				schemaProperties = {
+					@SchemaProperty(
+						name = "isSuccess",
+						schema = @Schema(type = "boolean", example = "false")
+					),
+					@SchemaProperty(
+						name = "code",
+						schema = @Schema(type = "string", example = "PAYMENT_ORDER4001")
+					),
+					@SchemaProperty(
+						name = "message",
+						schema = @Schema(type = "string", example = "해당 결제 주문이 존재하지 않습니다.")
+					),
+					@SchemaProperty(
+						name = "result",
+						schema = @Schema(type = "list", example = "{}")
+					)
+				}
+			)
+		),
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "PAYMENT4001",
+			description = "실 결제 금액과 결제 주문 금액이 다릅니다. 위변조 되었을 수 있습니다.",
+			content = @Content(
+				mediaType = MediaType.APPLICATION_JSON_VALUE,
+				schemaProperties = {
+					@SchemaProperty(
+						name = "isSuccess",
+						schema = @Schema(type = "boolean", example = "false")
+					),
+					@SchemaProperty(
+						name = "code",
+						schema = @Schema(type = "string", example = "PAYMENT4001")
+					),
+					@SchemaProperty(
+						name = "message",
+						schema = @Schema(type = "string",
+							example = "실 결제 금액과 결제 주문 금액이 다릅니다. 위변조 되었을 수 있습니다.")
+					),
+					@SchemaProperty(
+						name = "result",
+						schema = @Schema(type = "list", example = "{}")
+					)
+				}
+			)
+		)
+	})
 	@PostMapping("/payment")
 	public ApiResponse<PaymentCompleteResponse> completePayment(@RequestBody PaymentVerificationRequest request) throws
 		IamportResponseException,
