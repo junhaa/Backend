15059e947950383d3252738c0e0a4cd32198afa4
junhaa <2171326@hansung.ac.kr>
2024-02-08 15:03:21 +0900

    :art: CHORE. 충돌 해결 #7

--- src/main/java/com/bid/auction/global/enums/statuscode/ErrorStatus.java
+++ src/main/java/com/bid/auction/global/enums/statuscode/ErrorStatus.java
@@ -31,7 +31,6 @@ public enum ErrorStatus implements BaseCode {
 	_NOT_POSITIVE_NUMBER(HttpStatus.BAD_REQUEST, "AUCTIONPOST4004", "음수일 수 없습니다."),
 	_INVALID_ENUM_VALUE(HttpStatus.BAD_REQUEST, "AUCTIONPOST4005", "지원하지 않는 필드 값입니다."),
 	_AUCTION_POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "AUCTIONPOST4006", "유효하지 않은 상품 게시물입니다."),
-	_INVALID_ENUM_VALUE(HttpStatus.BAD_REQUEST, "AUCTIONPOST4005", "지원하지 않는 필드 값입니다."),
 
 	REDIS_ERROR(HttpStatus.BAD_REQUEST, "REDIS ERROR", "REDIS ERROR"),
 	INVALID_JWT(HttpStatus.BAD_REQUEST, "INVALID_JWT", "INVALID_JWT"),
@@ -60,7 +59,13 @@ public enum ErrorStatus implements BaseCode {
 		return message;
 	}
 
-	public HttpStatus getHttpStatus() {
+	@Override
+	public HttpStatus getStatus() {
 		return httpStatus;
+	}
+
+	@Override
+	public Integer getStatusValue() {
+		return httpStatus.value();
 	}
 }
